3ee48d5de49aa8bd96d53dc292f834fba8069a15
ALX <alexmonac13@gmail.com>
2023-07-18 22:12:34 +0200

    Initial Commit

---
+++ AlertComponent/AlertComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertComponentApp.swift
+//  AlertComponent
+//
+//  Created by Alex on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlertComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
